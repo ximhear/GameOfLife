82a3032f123854dd225087763d9a8c3d10b61ddf
C.H Lee <ximhear@gmail.com>
2024-12-01 00:07:40 +0200

    Initial Commit

---
+++ GameOfLine/GameOfLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLineApp.swift
+//  GameOfLine
+//
+//  Created by gzonelee on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
